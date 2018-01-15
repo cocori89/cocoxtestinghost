fe4d2742adf9118aee54503fe5c2e03e5c055797
cocori89 <33439195+cocori89@users.noreply.github.com>
2018-01-15 13:47:26 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,14 @@
+    $(function () {
+        $(".bit-nav:nth-of-type(1)").on("click", function () {
+            location.href = "bitDictionary.html";
+        });
+        $(".bit-nav:nth-of-type(2)").on("click", function () {
+            location.href = "bitInvestment.html";
+        });
+        $(".bit-nav:nth-of-type(3)").on("click", function () {
+            location.href = "bitExchange.html";
+        });
+        $(".bit-nav:nth-of-type(4)").on("click", function () {
+            location.href = "bitInvestment.html";
+        });
+    });
